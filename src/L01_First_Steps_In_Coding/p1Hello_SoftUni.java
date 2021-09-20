13283248c9bfe4192ca5fa7336ef8c48ddca4181
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-09-20 11:21:12 +0300

    с

--- src/L01_First_Steps_In_Coding/p1Hello_SoftUni.java
+++ src/L01_First_Steps_In_Coding/p1Hello_SoftUni.java
@@ -4,6 +4,7 @@ public class p1Hello_SoftUni {
     public static void main(String[] args) {
         System.out.println("Hello SoftUni");
         // write your code here
+        //тест
     }
 }
 
